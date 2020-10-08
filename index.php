12fbe20fcadd52122b097f144ad797f5937068b4
Jekoyu <jekoyu157@gmail.com>
2020-10-08 18:20:10 +0700

    membuat index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Git</title>
+</head>
+<body>
+    <h3>
+        <ul>
+            <li>Adi Prasetya</li>
+            <li>Susilo Pamungkas</li>
+        </ul>
+    </h3>
+</body>
+</html>
