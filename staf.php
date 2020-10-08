81a8eeb28188566f8d556c1528bedd4c367351bb
Jekoyu <jekoyu157@gmail.com>
2020-10-08 18:45:34 +0700

    menambahkan file staf

---
+++ staf.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Staf</title>
+</head>
+<body>
+    <h3>Data Staf Dan Karyawan</h3> 
+    <ul>
+        <li>Eko Prasetyo</li>
+        <li> Bambang</li>
+    </ul>
+</body>
+</html>
